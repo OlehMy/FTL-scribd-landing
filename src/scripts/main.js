995fc1a6e63c0969741f5b8d897fa7062d879b8d
Oleh Mykolaichuk <ooolehmy@gmail.com>
2022-12-02 06:30:27 +0200

    catalog section added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,49 @@
 'use strict';
+import Swiper, { Scrollbar } from 'swiper';
+import 'swiper/swiper-bundle.min.css';
 
+// menu
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--menu-is-open');
   } else {
     document.body.classList.remove('page__body--menu-is-open');
   }
+});
+
+//swiper
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  grabCursor: true,
+  slideToClickedSlide: true,
+  centeredSlides: true,
+  slidesPerView: 1,
+
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+
+  breakpoints: {
+    400: {
+      slidesPerView: 1.3,
+    },
+    500: {
+      slidesPerView: 1.6,
+    },
+    600: {
+      slidesPerView: 2,
+    },
+    920: {
+      slidesPerView: 3,
+    },
+    1250: {
+      slidesPerView: 4,
+    },
+    1800: {
+      slidesPerView: 6,
+    }
+  },
+
+  modules: [Scrollbar],
 });
