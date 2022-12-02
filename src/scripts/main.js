9432901ec5930a0ef1e0cca305481ee4dec9daa0
Oleh Mykolaichuk <ooolehmy@gmail.com>
2022-12-02 17:25:50 +0200

    footer was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -55,3 +55,31 @@ form.addEventListener('submit', e => {
   e.preventDefault();
   e.target.reset();
 });
+
+//select
+const select = function () {
+  let selectHeader = document.querySelectorAll('.select__header');
+  let selectItem = document.querySelectorAll('.select__item');
+
+  selectHeader.forEach(item => {
+    item.addEventListener('click', selectToggle)
+  });
+
+  selectItem.forEach(item => {
+    item.addEventListener('click', selectChoose)
+  });
+
+  function selectToggle() {
+    this.parentElement.classList.toggle('is-active');
+  }
+
+  function selectChoose() {
+    let text = this.innerText,
+      select = this.closest('.select'),
+      currentText = select.querySelector('.select__current');
+    currentText.innerText = text;
+    select.classList.remove('is-active');
+  }
+};
+
+select();
